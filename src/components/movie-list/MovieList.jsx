92f8016238cd9b493bfa254b1a106e9724114c0f
alvaroun <alvaromachadoferreira@hotmail.com>
2021-10-22 02:58:10 -0300

    up

--- src/components/movie-list/MovieList.jsx
+++ src/components/movie-list/MovieList.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 import './movie-list.scss';
 
 import { SwiperSlide, Swiper } from 'swiper/react';
-import { Link } from 'react-router-dom';
 
-import Button from '../button/Button';
+
+
 
 import tmdbApi, { category } from '../../api/tmdbApi';
-import apiConfig from '../../api/apiConfig';
+
 
 import MovieCard from '../movie-card/MovieCard';
 
